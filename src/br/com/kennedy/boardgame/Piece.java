f7f41fc05c878f6e0b7e7633bcd22e4567058416
Ray Kennedy <ray.knd@gmail.com>
2020-05-09 20:01:47 -0300

    Starting to implement Board and Piece

---
+++ src/br/com/kennedy/boardgame/Piece.java
@@ -0,0 +1,16 @@
+package br.com.kennedy.boardgame;
+
+public class Piece {
+	protected Position position; 
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
+}
