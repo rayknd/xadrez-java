bb3509840b0e7d0471674739258a636077b3d26f
Ray Kennedy <ray.knd@gmail.com>
2020-05-18 13:30:55 -0300

    Possible moves of a piece

--- src/br/com/kennedy/chess/ChessPiece.java
+++ src/br/com/kennedy/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package br.com.kennedy.chess;
 import br.com.kennedy.boardgame.Board;
 import br.com.kennedy.boardgame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 	private Color color;
 
 	public ChessPiece(Board board, Color color) {
