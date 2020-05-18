63d6171093a7486da90536e78c895416d1d1efc9
Ray Kennedy <ray.knd@gmail.com>
2020-05-18 11:03:26 -0300

    Moving pieces

--- src/br/com/kennedy/application/Program.java
+++ src/br/com/kennedy/application/Program.java
@@ -1,15 +1,32 @@
 package br.com.kennedy.application;
 
+import java.util.Scanner;
+
 import br.com.kennedy.chess.ChessMatch;
+import br.com.kennedy.chess.ChessPiece;
+import br.com.kennedy.chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
-		//Responsável exibir o tabuleiro
-		UI.printBoard(chessMatch.getPieces());
+		while(true) {
+			//Responsável exibir o tabuleiro
+			UI.printBoard(chessMatch.getPieces());
+			
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 
 }
--- src/br/com/kennedy/chess/ChessException.java
+++ src/br/com/kennedy/chess/ChessException.java
@@ -1,6 +1,8 @@
 package br.com.kennedy.chess;
 
-public class ChessException extends RuntimeException {
+import br.com.kennedy.boardgame.BoardException;
+
+public class ChessException extends BoardException {
 
 	private static final long serialVersionUID = 1L;
 
